bea8b6ac334f0828b8be8e7ce4c4a7f1c5837630
Adham Gaweesh <adhamgaweesh@gmail.com>
2026-09-08 13:00:43 +0300

    remove old

--- task_3/Items/__init__.py
+++
@@ -1,7 +0,0 @@
-from .status_enum import ItemStatus
-from .base_item import LibraryItem
-from .book import Book
-from .dvd import DVD
-from .magazine import Magazine
-
-__all__ = ["ItemStatus", "LibraryItem", "Book", "DVD", "Magazine"]
--- task_3/Items/status_enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class ItemStatus(Enum):
-    AVAILABLE = "Available"
-    CHECKED_OUT = "Checked Out"
-    LOST = "Lost"
